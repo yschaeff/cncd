1193b94bb981543abe8cb74d5b3dbda1884b1428
Yuri Schaeffer <yuri@nlnetlabs.nl>
2019-06-11 22:38:46 +0200

    A misbehaving shell may not hang the daemon by not yielding in time.

--- plugins/shell.py
+++ plugins/shell.py
@@ -1,13 +1,17 @@
-import subprocess, shlex
+import subprocess, shlex, asyncio
 import logging as log
 from pluginmanager import Action
 from plugins.pluginskel import SkeletonPlugin
+from functools import partial
+import concurrent.futures
 
 ## Friendly reminder ##
 ## This plugin is safe if-and-only-if the enduser is connected via an
 ## SSH tunnel, thereby proving it has the needed credentials.
 ## This plugin MUST NOT be used when a client is potentially unprivileged,
 ## such as with a http client. Or using a TCP socket instead of Unix socket.
+
+MAX_TIME_SUBPROCESS = 3
 
 class Plugin(SkeletonPlugin):
     """Inherit from this class for all Plugins"""
@@ -30,8 +34,22 @@ class Plugin(SkeletonPlugin):
     async def handle_command(self, gctx, cctx, lctx):
         if len(lctx.argv) != 2:
             return "specify 1 command exactly"
-        out = subprocess.run(shlex.split("{}".format(lctx.argv[1])),
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        def shell(action):
+            return subprocess.run(action, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        action = shlex.split("{}".format(lctx.argv[1]))
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        loop = self.gctx['loop']
+        blocking_task = loop.run_in_executor(executor, shell, action)
+        #try:
+        out = await asyncio.wait_for(blocking_task, timeout=None)
+        #except concurrent.futures._base.TimeoutError:
+            #blocking_task.cancel()
+            #try:
+                #await blocking_task
+            #except asyncio.CancelledError:
+                #return "mine"
+            #return "Subprocess took more than {} seconds. Killed.".format(MAX_TIME_SUBPROCESS)
+
 
         msg = {'stdout':str(out.stdout, encoding='utf-8'),
                'stderr':str(out.stderr, encoding='utf-8'),
