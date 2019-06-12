9ff321a6a9e6ca34927a1f6e3bb7e89e5e6528e7
Yuri Schaeffer <yuri@nlnetlabs.nl>
2019-06-12 08:16:06 +0200

    cleanup

--- plugins/shell.py
+++ plugins/shell.py
@@ -11,8 +11,6 @@ import concurrent.futures
 ## This plugin MUST NOT be used when a client is potentially unprivileged,
 ## such as with a http client. Or using a TCP socket instead of Unix socket.
 
-MAX_TIME_SUBPROCESS = 3
-
 class Plugin(SkeletonPlugin):
     """Inherit from this class for all Plugins"""
     NAME = "shell"
@@ -34,22 +32,18 @@ class Plugin(SkeletonPlugin):
     async def handle_command(self, gctx, cctx, lctx):
         if len(lctx.argv) != 2:
             return "specify 1 command exactly"
+
         def shell(action):
             return subprocess.run(action, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
         action = shlex.split("{}".format(lctx.argv[1]))
         executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
         loop = self.gctx['loop']
         blocking_task = loop.run_in_executor(executor, shell, action)
-        #try:
-        out = await asyncio.wait_for(blocking_task, timeout=None)
-        #except concurrent.futures._base.TimeoutError:
-            #blocking_task.cancel()
-            #try:
-                #await blocking_task
-            #except asyncio.CancelledError:
-                #return "mine"
-            #return "Subprocess took more than {} seconds. Killed.".format(MAX_TIME_SUBPROCESS)
-
+        try:
+            out = await asyncio.wait_for(blocking_task, timeout=None)
+        except FileNotFoundError as e:
+            return str(e)
 
         msg = {'stdout':str(out.stdout, encoding='utf-8'),
                'stderr':str(out.stderr, encoding='utf-8'),
